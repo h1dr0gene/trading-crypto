f310fa56b4d0755e8f8bf6d52f73b57b422b8b20
h1dr0gene <115615027+h1dr0gene@users.noreply.github.com>
2023-03-02 10:46:26 +0100

    Update scrypt.py

--- scrypt.py
+++ scrypt.py
@@ -4,8 +4,8 @@ import pandas_ta as ta
 
 #create dataset 
 
-klinesT = Client().get_historical_klines("BTCUSDT",Client.KLINE_INTERVAL_1HOUR, "01 January 2017")
-df = pd.DataFrame(klinesT, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore' ])
+klinesT = Client().get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, "01 January 2017")
+df = pd.DataFrame(klinesT, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
 
 # cleaning dataset
 
@@ -16,10 +16,10 @@ del df['trades']
 del df['tb_base_av']
 del df['tb_quote_av']
 
-df ['close'] = pd.to_numeric(df['close'])
-df ['high'] = pd.to_numeric(df['high'])
-df ['low'] = pd.to_numeric(df['low'])
-df ['open'] = pd.to_numeric(df['open'])
+df['close'] = pd.to_numeric(df['close'])
+df['high'] = pd.to_numeric(df['high'])
+df['low'] = pd.to_numeric(df['low'])
+df['open'] = pd.to_numeric(df['open'])
 
 #convert time 
 df = df.set_index(df['timestamp'])
@@ -27,37 +27,33 @@ df.index = pd.to_datetime(df.index, unit='ms')
 del df['timestamp']
 
 #indicator defining 
-df['SMA20'] = ta.sma(df['close'],length=200)
+df['SMA20'] = ta.sma(df['close'], length=200)
 df['SMA60'] = ta.sma(df['close'], length=600)
 
 #stratégie backtest 
 print(df)
-t=0
-x=0
-usdt=1000
+usdt = 1000
 btc = 0
-lastIndex =  df.first_valid_index()
+lastIndex = df.first_valid_index()
+buy_price = None
 
 for index, row in df.iterrows():
-    x=x+1
     if df['SMA20'][lastIndex] > df['SMA60'][lastIndex]:
-        t=t+1
         if btc == 0:
             btc = usdt / df['close'][index]
-            usdt = 0 
-            print("BUY BTC at",df['close'][index],'$ the', index) 
-    elif df['SMA20'][lastIndex]< df['SMA60'][lastIndex]:
-        t=t+1
-        if usdt == 0:
+            usdt = 0
+            buy_price = df['close'][index]
+            print("BUY BTC at", buy_price, '$ the', index) 
+    elif df['SMA20'][lastIndex] < df['SMA60'][lastIndex]:
+        if btc > 0:
             usdt = btc * df['close'][index]
-            btc = 0 
-            print("SELL BTC at",df['close'][index],'$ the', index)
-    else: 
-        lastIndex = index  # Update lastIndex for the current iteration.
+            btc = 0
+            sell_price = df['close'][index]
+            print("SELL BTC at", sell_price, '$ the', index)
+            print("Profit:", sell_price / buy_price)
+    lastIndex = index
 
 #test and final result 
 finalResult = usdt + btc * df['close'].iloc[-1]
 print('Final result',finalResult,"USDT")
-print('buy and hold result',(1000 / df['close'].iloc[0]) * df['close'].iloc[-1],'USDT')
-print ('line take in count = ',x)
-print ('trade =',t)
+print('buy and hold result', (1000 / df['close'].iloc[0]) * df['close'].iloc[-1], 'USDT')
